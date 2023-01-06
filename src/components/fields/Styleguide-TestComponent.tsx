1b196bafce8255261f99b17e7a067c6aa8d739cd
Ben Norman <bno@codehousegroup.com>
2023-01-06 19:15:52 +1100

    implement Product List TestComponent

--- src/components/fields/Styleguide-TestComponent.tsx
+++ src/components/fields/Styleguide-TestComponent.tsx
@@ -1,13 +1,44 @@
 import OcProductList from '../../ordercloud/components/OcProductList';
+import OcProductCard from '../../ordercloud/components/OcProductCard';
+import Link from 'next/link';
+import { BuyerProduct } from 'ordercloud-javascript-sdk';
+import useNextRouterMapping, { NextQueryMap } from '../../ordercloud/hooks/useNextRouterMapping';
 
+const queryMap: NextQueryMap = {
+  search: 's',
+  page: 'p',
+  pageSize: 'ps',
+  searchOn: 'so',
+  sortBy: 'o',
+  'xp.size': 'size',
+  'xp.color': 'color',
+  'xp.test_boolean': 'bool',
+  'xp.test_number': 'num',
+};
+
+const TestComponent = () => {
 /**
  * Demonstrates usage of a Text content field within JSS.
  * Text fields are HTML encoded by default.
  */
-const TestComponent = () => (
-  <div>
-    <OcProductList />
-  </div>
-);
+
+  const { isReady, options, updateQuery } = useNextRouterMapping(queryMap);
+
+  const handleRenderItem = (p: BuyerProduct) => {
+    return (
+      <Link href={`/products/${p.ID}`}>
+        <a>
+          <OcProductCard product={p} />
+        </a>
+      </Link>
+    );
+  };
+
+  return (
+    <div>
+      <OcProductList options={options} renderItem={handleRenderItem} />
+    </div>
+  );
+};
 
 export default TestComponent;
