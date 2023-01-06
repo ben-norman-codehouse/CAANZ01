f6c23f342a87e3a4b3bc2613a33bc15ef21339ad
CODEHOUSEGROUP\ama <ama@codehousegroup.com>
2023-01-06 09:06:48 +0530

    FE initial

---
+++ src/components/fields/Styleguide-TestComponent.tsx
@@ -0,0 +1,7 @@
+/**
+ * Demonstrates usage of a Text content field within JSS.
+ * Text fields are HTML encoded by default.
+ */
+const TestComponent = () => <div>Hello dfbdfbdb</div>;
+
+export default TestComponent;
