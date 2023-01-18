cb555736a0f0b60d679737f438c38b1c0965c3ae
Ben Norman <bno@codehousegroup.com>
2023-01-18 17:02:22 +1100

    change the name for the products

--- sitecore/definitions/components/products/ProductDetail-Layout.sitecore.ts
+++ sitecore/definitions/components/products/ProductDetail-Layout.sitecore.ts
@@ -1,7 +1,7 @@
 import { SitecoreIcon, Manifest } from '@sitecore-jss/sitecore-jss-dev-tools';
 
 /**
- * Adds the Product-Layout component to the disconnected manifest.
+ * Adds the Products-Layout component to the disconnected manifest.
  * This function is invoked by convention (*.sitecore.js) when 'jss manifest' is run.
  * @param {Manifest} manifest Manifest instance to add components to
  */
